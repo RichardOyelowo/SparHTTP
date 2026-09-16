e673c19920a6c5ae0fff3825a6c38e40538eebf8
Richard <richardadebowale.oye@gmail.com>
2026-09-16 15:01:01 -0700

    (feat): Define HTTP request and response data structures

--- c/http.h
+++ c/http.h
@@ -1,7 +1,16 @@
 #ifndef HTTP_H
 #define HTTP_H
 
+#include <stdio.h>
 #include <sys/socket.h>
+
+#define MAX_HEADER 10
+#define MAX_BODY 500
+
+typedef struct {
+    char name[64];
+    char value[500];
+} Header;
 
 typedef struct {
     char method[16];
@@ -9,9 +18,8 @@ typedef struct {
     char version[30];
 } RequestLine;
 
-
 ssize_t receive_request(int client_socket, char *buffer, size_t buffer_size);
-RequestLine parse_request(char *buffer, ssize_t buffer_length);
+int parse_request(char *buffer, RequestLine *request, Header *header, char **body);
 
 
 #endif
