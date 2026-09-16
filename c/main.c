22080a70b889e5f3e559b38dd2cdf87ccb465f00
Richard <richardadebowale.oye@gmail.com>
2026-09-16 15:01:40 -0700

    (feat): Handle parsed HTTP request data

--- c/main.c
+++ c/main.c
@@ -7,7 +7,6 @@
 
 #define BUFFER_SIZE 4096
 
-
 int main() {
     printf("Welcome to SparHTTP\n");
     int server_socket = create_server();
@@ -15,15 +14,30 @@ int main() {
 
     ssize_t bytes_length;
     char buffer[BUFFER_SIZE];
+
+    // buffer parsing & handling
     RequestLine http_request;
- 
+    Header header[MAX_HEADER];
+    char *client_msg;
+    int header_count;
+
     bytes_length = receive_request(client_socket, buffer, sizeof(buffer));
+    header_count = parse_request(buffer, &http_request, header, &client_msg);
 
+    // print parsed infos
+    printf("Reading %zd bytes...\n\n", bytes_length);
 
-    printf("Read %zd bytes\n\n", bytes_length);
     printf("HTTP Request method: %s\n", http_request.method);
     printf("HTTP Request path: %s\n", http_request.path);
     printf("HTTP Request version: %s\n", http_request.version);
+
+    // printing header infos
+    for (int i = 0; i < header_count; i++) {
+        printf("%s: %s\n", header[i].name, header[i].value);
+    }
+
+    // message body 
+    printf("Client message: %s\n", client_msg);
 
     return 0;
 }
