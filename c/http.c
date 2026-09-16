7d99be0834340a50139dfee6021f545fb3d4cb02
Richard <richardadebowale.oye@gmail.com>
2026-09-16 15:01:29 -0700

    (feat): Parse HTTP request headers and body

--- c/http.c
+++ c/http.c
@@ -3,12 +3,7 @@
 #include <unistd.h>
 #include <stddef.h>
 #include <sys/socket.h>
-
-typedef struct {
-    char method[16];
-    char path[256];
-    char version[16];
-} RequestLine;
+#include "http.h"
 
 
 ssize_t receive_request(int client_socket, char *buffer, size_t buffer_size) {
@@ -17,7 +12,7 @@ ssize_t receive_request(int client_socket, char *buffer, size_t buffer_size) {
     if (nread < 0) {
         perror("recv");
     } else {
-        // writes the last character to prevent running into error with bytes characters
+        // last character to prevent running into error with bytes char
         buffer[nread] = '\0';
     }
 
@@ -25,41 +20,62 @@ ssize_t receive_request(int client_socket, char *buffer, size_t buffer_size) {
 }
 
 
-RequestLine parse_request(char *buffer, ssize_t buffer_length) {
+int parse_request(char *buffer, RequestLine *request_line, Header *header, char **body) {
     char *current = buffer;
-    int line = 0;
-    char temp[256];
-    RequestLine http_request;
+    int line_count = 0;
+    int header_count = 0;
    
     while(current[0] != '\r' || current[1] != '\n') {
         char *end = strchr(current, '\r');
+        if (end == NULL)
+            break;
+
         *end = '\0';
-        size_t temp_len = end - current;
 
         // Request line infos handling
-        if (line == 0) {
+        if (line_count == 0) {
             char *first_space = strchr(current, ' ');
             size_t method_len = first_space - current;
-            memcpy(http_request.method, current, method_len);
-            http_request.method[method_len] = '\0';
+            memcpy(request_line->method, current, method_len);
+            request_line->method[method_len] = '\0';
             current = first_space + 1;
 
             char *second_space = strchr(current, ' ');
             size_t path_len = second_space - current;
-            memcpy(http_request.path, current, path_len);
-            http_request.path[path_len] = '\0';
+            memcpy(request_line->path, current, path_len);
+            request_line->path[path_len] = '\0';
             current = second_space + 1;
 
             size_t version_len = end - current;
-            memcpy(http_request.version, current, version_len);
-            http_request.version[version_len] = '\0';
+            memcpy(request_line->version, current, version_len);
+            request_line->version[version_len] = '\0';
+
+            // updates current pointer to next line start
+            current = end + 2;
+
+            line_count++;
+        }else {
+            //parsing header, using line since it's one header -> value per line
+            char *colon = strchr(current, ':');
+            if (colon == NULL)
+                break;
+
+            size_t header_len = colon - current;
+            memcpy(header[header_count].name, current, header_len);
+            header[header_count].name[header_len] = '\0';
+            current = colon + 2;
+
+            size_t value_len = end - current;
+            memcpy(header[header_count].value, current, value_len);
+            header[header_count].value[value_len] = '\0';
+            current = end + 2;
+
+            // updates line and current to next buffer line
+            header_count++;
         }
-        memcpy(temp, current, temp_len);
-
-        // updates line and current to next buffer line
-        line++;
-        current = end + 2;
     }
+    current += 2;
+    *body = current;
 
-    return http_request;
+    return header_count;
 }
